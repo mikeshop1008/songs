8804c7099cf0f167d81187ac3c21a5858f176758
mikeshop1008 <mikeshop1008@gmail.com>
2026-02-26 23:50:49 +0800

    Refactor repo to standalone KSC-ARM single-file project

--- Core/Src/stm32f4xx_hal_msp.c
+++
@@ -1,9 +0,0 @@
-#include "stm32f4xx_hal.h"
-
-void HAL_MspInit(void)
-{
-    __HAL_RCC_SYSCFG_CLK_ENABLE();
-    __HAL_RCC_PWR_CLK_ENABLE();
-
-    HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-}
